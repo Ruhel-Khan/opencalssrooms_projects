c0b4a05cc791676d7c272523b19d23ed89207925
Ruhel-Khan <ruhelk777@gmail.com>
2021-10-21 14:22:01 +0100

    fully working script to scrape all urls of a category and write to cvs file

--- category_scraper.py
+++ category_scraper.py
@@ -13,19 +13,16 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 #create lists to store the page items
 books = []
-header = []
-#target outer tag
-list_items = soup.find_all('article', class_='product_pod')
 
-#loop through to find inner tag and append to list
-for item in list_items:
-    link = list_items.find('a').get('href')
-    print(link)    #books.append(link)
-'''
+#get the product page urls
+products_urls = [x.div.a.get('href') for x in soup.findAll("article", class_ = "product_pod")]
+
+#append urls to list
+for url in products_urls:
+    books.append(url)
+
 #open csv file and write data to file
 with open('category.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
-    writer.writerow(header)
     row = books
     writer.writerow(row)
-'''
