5559ab2a93a9f451e85f09778d5127569b46ae30
Ruhel-Khan <ruhelk777@gmail.com>
2021-11-09 12:37:29 +0000

    scrapes the whole site by category and outputs to csv, initial commit, does not work properly yet

--- site_scraper.py
+++ site_scraper.py
@@ -1,79 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-import time
-
-#create empty lists for all the site information
-category = []
-product_url = []
-title = []
-upc = []
-price_inc_tax = []
-price_ex_tax = []
-numbers_available = []
-review_rating = []
-image_url = []
-description = []
+from urllib.parse import urljoin
 
 
-#loop for pagination range
-for x in range(1, 100):
-    print("scraping page.."+str(x))
+categories = []
+sliced_catgories = []
 
-    #set url to scrape
-    url = 'https://books.toscrape.com/catalogue/page-'
 
-    #get page content
-    page = requests.get(url+str(x) + ".html")
 
-    #parse page content into a soup object
-    soup = BeautifulSoup(page.content, 'html.parser')
+#set base url and return the page html content into a soup object
+url = "https://books.toscrape.com/"
+page = requests.get(url)
+soup = BeautifulSoup(page.text, 'lxml')
+#get all the categories from the side bar section
+all_categories = soup.find_all('li', limit = 10)
+del all_categories[:3]
+#append category urls to list
+for i in all_categories:
+    links = i.find('a', href=True)
+    categories.append(links['href'])
 
-    #get the product page urls
-    products_urls = [x.div.a.get('href') for x in soup.findAll("article", class_ = "product_pod")]
 
-    #get the book category
-    get_category = soup.find_all("a")
-    if get_category not in category:
-        category.append(get_category[3].text)
-    else:
-        continue
-    print(category)
+for category in categories:
+    cat_page = urljoin(url , category)
+    page = requests.get(cat_page)
+    soup = BeautifulSoup(page.text, 'lxml')
+    #print(soup.find('h1').text)
+    #next_page_element = soup.select_one('li.next > a')
 
-    #append title to data list
-    title.append(soup.find("h1").text)
-
-    #get all the table data
-    table_data = soup.find_all("td")
-
-    #append all the table data into the data list
-    for i in table_data:
-        if ((i.text == "Books") or (i.text == "£0.00") or (i.text == "0")):
-            continue
-        data.append(i.text)
-
-    #append urls to list
-    for url in products_urls:
-        product_url.append(url)
-
-    #get the product Description
-    product_desc = soup.find_all("p")
-    description.append(product_desc[3].text)
-
-    #open csv file and write data to file
-    with open('site.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',')
-        row = product_url
-        writer.writerow(row)
-
-    #get the next next page button tag
-    pager = soup.find_all('ul', class_= "pager")
-    next_btn = soup.find('li', class_= "next")
-
-    #set page exception
-    if next_btn == None:
-        print(str(x) + " " + "Pages scraped")
-        break
-
-    #set a small time delay
-    #time.sleep(1)
+    for page in category:
+        print(soup.find('h1'))
+        next_page_element = soup.select_one('li.next > a')
+        if next_page_element:
+            next_page_url = next_page_element.get('href')
+            page_url = urljoin(cat_page, next_page_url)
+            page = requests.get(page_url)
+            soup = BeautifulSoup(page.text, 'lxml')
+            #print(soup.find('h1'))
+        else:
+            break
