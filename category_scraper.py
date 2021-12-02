302cae3c4db9a5f80c1ebfb0e1d89b5e79ae0963
Ruhel-Khan <ruhelk777@gmail.com>
2021-12-02 21:08:56 +0000

    cleaned up and refractored

--- category_scraper.py
+++ category_scraper.py
@@ -1,28 +1,35 @@
+
+#import required modules
 import requests
 from bs4 import BeautifulSoup
 import csv
 
-#set url to scrape
-url = 'http://books.toscrape.com/catalogue/category/books/travel_2/index.html'
-
-#get page content
-page = requests.get(url)
-
-#parse page content into a soup object
-soup = BeautifulSoup(page.content, 'html.parser')
-
 #create lists to store the page items
 books = []
 
-#get the product page urls
-products_urls = [x.div.a.get('href') for x in soup.findAll("article", class_ = "product_pod")]
+#set url to scrape
+url = "http://books.toscrape.com/catalogue/category/books/travel_2/index.html"
+#parse html into a beautifulSoup object
+def get_soup(url):
+    #get page content
+    page = requests.get(url)
+    #parse page content into a soup object
+    soup = BeautifulSoup(page.content, 'html.parser')
+    return soup
+soup = get_soup(url)
 
-#append urls to list
-for url in products_urls:
-    books.append(url)
+ #get all the product urls
+def product_urls(soup):
+    list_items = soup.find_all('article', 'product_pod')
+    for item in list_items:
+        link = item.find('a').get('href')
+        books.append(link)
+product_urls(soup)
 
-#open csv file and write data to file
-with open('category.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',')
-    row = books
-    writer.writerow(row)
+#write to csv file
+def write_to_csv():
+    with open('category.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        row = books
+        writer.writerow(row)
+write_to_csv()
