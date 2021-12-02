6b07100e95529bdf15c5425c9c219f88c3d9ea8a
Ruhel-Khan <ruhelk777@gmail.com>
2021-12-02 21:07:54 +0000

    refractored and cleaned up

--- book_scraper.py
+++ book_scraper.py
@@ -14,17 +14,41 @@ page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 #print(soup)
 
+#create a list of all the headers
+headers = ["Product Page URL", "UPC", "Title", "Price Including Tax", "Price Excluding Tax", "Numbers Available", "Product Description", "Category", "Review Rating", "Image URL" ]
+#create a list of all the extracted data
+data = []
+
 #get product page url
 product_page_url = url
+data.append(product_page_url)
+#print("Product Page URL: ", product_page_url)
+
 #get all the info in the html table
 info_table = soup.find("table", class_="table table-striped")
 
-print(info_table.text.strip())
+#print(info_table.text)
 
-#loop through the table data
-
+#loop through table data
 for i in info_table.find_all('tbody'):
     rows = i.find_all('tr')
+    data = row.find('td')
     for row in rows:
-        data = row.find('td')
-        print(data)
+        print(row)
+
+#get book book_title#
+book_title = soup.find("h1")
+print("Book Title: ", book_title.text.strip())
+
+#get book description
+product_desc = soup.find_all("p")
+print("Product Description: ", product_desc[3].text)
+
+#get the image url
+image_url = soup.find_all("img")
+image = image_url[0]
+print("Image URL: ", image.attrs['src'])
+
+#get the book category
+category = soup.find_all("a")
+print("Category: ", category[3].text)
